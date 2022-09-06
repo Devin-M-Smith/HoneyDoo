abe8ccbcf3e6ec5b9d0447d7770daa929b0cd43f
Devin Smith <91283760+Devin-M-Smith@users.noreply.github.com>
2022-09-06 16:09:04 -0400

    Edit-Tasks (#27)
    * Fixed bug where selected task on Full Tasks would crash Edit Task on Main
    
    * Can edit partner tasks, change assignee, removed ability to complete tasks not assigned to self

--- tools/screens.py
+++ tools/screens.py
@@ -39,15 +39,21 @@ class RegisterContent(Label):
     pass
 
 class MainWindow(Screen):
+
     def on_pre_enter(self):
         self.ids.tasks.clear_widgets()
+        self.ids.selected.text = 'SELECTED TASK: NONE'
         try:
             c = config.mydb.cursor(buffered=True)
             c.reset()
         except:
             config.mydb = HoneyDooSQL.dbSetup()
-
-        config.task = HoneyDooSQL.readTasks(config.mydb)
+        if config.displayMode == 0:
+            config.task = HoneyDooSQL.readTasks(config.mydb)
+            config.currentName = config.name
+        else:
+            config.task = HoneyDooSQL.readPartnerTasks(config.mydb)
+            config.currentName = config.pairedName
 
     def completeTask(self):
         try:
@@ -55,21 +61,23 @@ class MainWindow(Screen):
             c.reset()
         except:
             config.mydb = HoneyDooSQL.dbSetup()
+        if config.displayMode == 0:
+            window = TaskPopUp(
+                title = "Mark Task as Complete?",
+                title_color = (.4, 1, .7, 1),
+                title_size = '28sp', 
+                separator_color = (0, .4, .2, 1),
+                auto_dismiss = False, 
+                background_color = (0, .4, .2, .5),
+                size_hint = (None, None), 
+                size = ('300sp', '150sp'))
+            window.open()
+        else:
+            global dataError
+            dataError = DataError()
+            wrongUserPopUp('You cannot mark a Task as complete unless it is assigned to you.')
 
-        window = TaskPopUp(
-            title = "Mark Task as Complete?",
-            title_color = (.4, 1, .7, 1),
-            title_size = '28sp', 
-            separator_color = (0, .4, .2, 1),
-            auto_dismiss = False, 
-            background_color = (0, .4, .2, .5),
-            size_hint = (None, None), 
-            size = ('300sp', '150sp'))
-        window.open()
-        pass
-
-    def on_enter(self):
-
+    def updatePage(self):
         i = 0
         for task in config.task:
             btn = TaskItem(size_hint_y=None, height='100sp')
@@ -98,6 +106,44 @@ class MainWindow(Screen):
         if i==0 :
             startLabel = RegisterContent(size_hint_y=None, height='100sp', font_size='20sp', text="Click + to create a New Task")
             self.ids.tasks.add_widget(startLabel)
+        self.ids.user.text = 'SHOWING ' + '[color=ffffff][b]'+config.currentName+'[/b][/color]' + "'S TASKS"
+
+    def switchUser(self):
+        if config.displayMode == 0:
+            MainWindow.showPartner(self)
+        else:
+            MainWindow.showSelf(self)
+
+    def showSelf(self):
+        config.displayMode = 0
+        self.ids.tasks.clear_widgets()
+        self.ids.user.text = 'PLEASE WAIT...'
+        self.ids.selected.text = 'SELECTED TASK: NONE'
+        try:
+            c = config.mydb.cursor(buffered=True)
+            c.reset()
+        except:
+            config.mydb = HoneyDooSQL.dbSetup()
+        config.task = HoneyDooSQL.readTasks(config.mydb)
+        config.currentName = config.name
+        MainWindow.updatePage(self)
+
+    def showPartner(self):
+        if config.paireduid.isdigit():
+            config.displayMode = 1
+            self.ids.tasks.clear_widgets()
+            self.ids.user.text = 'PLEASE WAIT...'
+            self.ids.selected.text = 'SELECTED TASK: NONE'
+            try:
+                c = config.mydb.cursor(buffered=True)
+                c.reset()
+            except:
+                config.mydb = HoneyDooSQL.dbSetup()
+            config.task = HoneyDooSQL.readPartnerTasks(config.mydb)
+            config.currentName = config.pairedName
+            MainWindow.updatePage(self)
+        else:
+            swappairedPopUp()
     pass
 
 class TaskPopUp(Popup):
@@ -125,10 +171,20 @@ class TaskItem(GridLayout):
         
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
+            config.taskTitle = self.ids.task_name.text
+            try:
+                self.parent.parent.parent.parent.ids.selected.text = 'SELECTED TASK: ' + config.taskTitle
+            except:
+                pass
             if self.height < kivy.metrics.sp(201):
                 self.grow(self)
                 self.ids.dropdown.grow(self.ids.dropdown)
             else:
+                try:
+                    self.parent.parent.parent.parent.ids.selected.text = 'SELECTED TASK: NONE'
+                except:
+                    pass
+                config.displayTask = 0
                 self.ids.dropdown.shrink(self.ids.dropdown)
                 self.shrink(self)
         else:
@@ -197,6 +253,7 @@ class UpdatePaired(Popup):
                     con.write(configfile)
                 config.pairedName = ''
                 config.paireduid = ''
+                config.displayMode = 0
                 return 'refreshSettings'
             else:
                 config.result = 'Invalid User Pairing Code'
@@ -208,6 +265,47 @@ class UpdatePaired(Popup):
             dataError = DataError()
             errorPopUp(config.result)
             return 'customSettings'
+    pass
+
+class UpdatePairedMain(Popup):
+    def paired(self, code):
+        if len(code) > 7:
+            try:
+                config.result = HoneyDooSQL.getUser(config.mydb, code)
+                con.read('config.ini')
+                con['PARTNER'] = {}
+                con['PARTNER']['name'] = config.result
+                con['PARTNER']['uid'] = code
+                config.pairedName = config.result
+                config.paireduid = code
+                HoneyDooSQL.updatePaired(config.mydb, code)
+                with open('config.ini','w') as configfile:
+                    con.write(configfile)
+                config.result = code
+                return
+            except:
+                config.result = 'User Not Found'
+        else:
+            if code == '0':
+                HoneyDooSQL.unPair(config.mydb)
+                con.read('config.ini')
+                con.remove_section('PARTNER')
+                with open('config.ini','w') as configfile:
+                    con.write(configfile)
+                config.pairedName = ''
+                config.paireduid = ''
+                config.displayMode = 0
+                return
+            else:
+                config.result = 'Invalid User Pairing Code'
+
+        if (config.result.isdigit()):
+            return
+        else:
+            global dataError
+            dataError = DataError()
+            errorPopUp(config.result)
+            return
     pass
 
 def pairedPopUp():
@@ -222,6 +320,16 @@ def pairedPopUp():
     window.open()
     pass
 
+def swappairedPopUp():
+    window = UpdatePairedMain(
+        title = "Pair New User",
+        title_color = (.4, 1, .7, 1),
+        title_size = '28sp', 
+        separator_color = (0, .4, .2, 1),
+        background_color = (0, .4, .2, .5),
+        size_hint = (None, None), 
+        size = ('350sp', '200sp'))
+    window.open()
 
 class TaskList(Screen):
 
@@ -268,6 +376,8 @@ class TaskList(Screen):
             self.ids.tasks.add_widget(btn)
             i += 1
         self.ids.topLabel.text = 'Full Task List'
+    def on_leave(self):
+        config.displayTask = 0
     pass
 
 
@@ -456,14 +566,23 @@ class NewTask(Screen):
 class EditTask(Screen):
     def on_pre_enter(self):
         print(config.displayTask)
+        con.read('config.ini')
+        usernames = []
+        for f in con.sections():
+            usernames.append(con[f]['name'])
+        self.ids.assigned.values = usernames
         self.ids.header.text = 'PLEASE WAIT...'
         self.ids.assigned.text = 'NO ASSIGNEE'
+        self.ids.task_name.hint_text = 'NO TASK SELECTED'
+        self.ids.description.hint_text = '\nPLEASE GO BACK AND SELECT A TASK'
         self.ids.task_name.text = ''
         self.ids.description.text = ''
         self.ids.priority.text = 'NO PRIORITY'
         if config.displayTask == 0:
             self.ids.header.text = 'UPDATE TASK'
         else:
+            self.ids.task_name.hint_text = 'TASK NAME'
+            self.ids.description.hint_text = '\nDESCRIPTION'
             try:
                 c = config.mydb.cursor(buffered=True)
                 c.reset()
@@ -475,8 +594,11 @@ class EditTask(Screen):
             self.ids.description.text = task[0]['DESCRIPTION']
             self.ids.priority.text = checkPriority(task[0]['PRIORITY'])
             self.ids.header.text = 'UPDATE TASK'
+    
+    def on_leave(self):
+        config.displayTask = 0
 
-    def submitTask(self, task_name, description, priority):
+    def submitTask(self, task_name, description, priority, userName):
 
         try:
             c = config.mydb.cursor(buffered=True)
@@ -488,25 +610,30 @@ class EditTask(Screen):
             priority = self.ids.priority.values.index(priority)
         except:
             priority = -1
-
-        if (
-            task_name == ''
-            or
-            description == ''
-            or
-            priority == -1):
-            result = 'Please fill out all fields'
+        if config.displayTask == 0:
+            result = 'Please go back and select a task.\nTo add a new task, please go back and select the + button.'
         else:
-            if config.displayTask == 0:
-                result = 'No task selected. Please go back and select a task.'
+            if (
+                task_name == ''
+                or
+                description == ''
+                ):
+                result = 'Please fill out all fields'
             else:
-                result = HoneyDooSQL.updateTask(config.mydb, task_name, description, priority, config.displayTask)
+                user = self.ids.assigned.values.index(userName)
+                if user == 0:
+                    result = HoneyDooSQL.updateTask(config.mydb, task_name, description, priority, config.displayTask, int(con['USER']['uid']))
+                else:
+                    result = HoneyDooSQL.updateTask(config.mydb, task_name, description, priority, config.displayTask, int(con['PARTNER']['uid']))
         if result == '':
             return 'main'
         else:
             global dataError
             dataError = DataError()
-            errorPopUp(result)
+            if config.displayTask != 0:
+                errorPopUp(result)
+            else:
+                noTasksPopup(result)
             return 'editTask'
     pass
 
@@ -528,4 +655,37 @@ def errorPopUp(result):
         background_color = (0, .4, .2, .5),
         size_hint = (None, None), 
         size = ('300sp', '150sp'))
+    window.open()
+
+def wrongUserPopUp(result):
+
+    dataError.ids.error.text = str(result)
+    show = dataError
+
+    window = Popup(
+        title = "Incorrect User",
+        title_color = (.4, 1, .7, 1),
+        title_size = '28sp', 
+        separator_color = (0, .4, .2, 1),
+        content = show, 
+        background_color = (0, .4, .2, .5),
+        size_hint = (None, None), 
+        size = ('300sp', '150sp'))
+    window.open()
+
+def noTasksPopup(result):
+
+    dataError.ids.error.text = str(result)
+    dataError.ids.error.pos_hint = ({'x': 0, 'top': 0.55})
+    show = dataError
+
+    window = Popup(
+        title = "No Task Selected",
+        title_color = (.4, 1, .7, 1),
+        title_size = '28sp', 
+        separator_color = (0, .4, .2, 1),
+        content = show, 
+        background_color = (0, .4, .2, .5),
+        size_hint = (None, None), 
+        size = ('320sp', '150sp'))
     window.open()
